5f74705763d1325fd3db52324b9a84a55db4fb06
dada542 <45128986+dada542@users.noreply.github.com>
2021-01-03 13:36:50 +0100

    Create Game.js

---
+++ Game.js
@@ -0,0 +1,32 @@
+const Game = function() {
+
+    this.colors = [0, 0, 0];
+    this.shifts = [1, 1, 1];
+
+    this.update = function() {
+
+        for(let i = 0; i < 3; i++) {
+
+            let color = this.colors[i];
+            let shift = this.shifts[i];
+
+            if(color + shift > 255 || color + shift < 0) {
+                shift = (shift < 0) ? Math.floor(Math.random() * 2 + 1) : Math.floor(Math.random() * -2 - 1)
+            }
+
+            color += shift;
+
+            this.colors[i] = color;
+            this.shifts[i] = shift;
+
+        }
+
+    }
+
+}
+
+Game.prototype = {
+
+    constructor: Game
+
+}
